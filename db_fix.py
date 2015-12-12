dbd29fb4ff067ac3efcd0159c96e26716913b0c9
Tracy Jones <tjones@vmware.com>
2015-12-12 14:59:51 -0800

    Initial update

--- db_fix.py
+++ db_fix.py
@@ -5,21 +5,29 @@ __author__ = 'tjones'
 import csv
 import json
 from optparse import OptionParser
+import os
 import pprint
 import requests
 import sys
 
 pp = pprint.PrettyPrinter()
 
-def import_data_file(inputfile):
-    with open(inputfile, 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        csvdata = []
-        for row in reader:
-            print(row['Name'], row['Place of Employment'])
-            firstname = row['Name'].split(' ')[0]
-            lastname = row['Name'].split(' ')[1]
-            csvdata.append({'contactFirstname': firstname, 'contactLastname': lastname, 'contactCompany': row['Place of Employment']})
+def import_data_file(input_dir):
+
+    csvdata = []
+    for dir_entry in os.listdir(input_dir):
+        if dir_entry.endswith('.csv'):
+            dir_entry_path = os.path.join(input_dir, dir_entry)
+            if os.path.isfile(dir_entry_path):
+                with open(dir_entry_path, 'rb') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        print(row['Name'], row['Place of Employment'])
+                        firstname = row['Name'].split(' ')[0]
+                        lastname = row['Name'].split(' ')[1]
+                        csvdata.append({'contactFirstname': firstname.strip(),
+                                        'contactLastname': lastname.strip(),
+                                        'contactCompany': row['Place of Employment'].strip()})
     return csvdata
 
 def login(username, password, account):
@@ -49,8 +57,6 @@ def update_work(updated_company_list, rg_data, csv_data):
             if csv['contactCompany'] != '':
                 if rg['contactLastname'] == csv['contactLastname']:
                     # TODO: not finding my own name...  something is wrong
-                    if rg['contactLastname'].lower() == 'jones':
-                        print 'FOUND IT'
                     if rg['contactFirstname'] == csv['contactFirstname']:
                         if rg['contactCompany'] != csv['contactCompany']:
                             rg['contactCompany'] = csv['contactCompany']
@@ -92,11 +98,25 @@ def merge_contacts(token, tokenHash, csv_data):
             print r.text
             return
 
+def store_data(output_file, csv_data):
+    with open(output_file, 'w') as csvfile:
+        fieldnames = ['first_name', 'last_name', 'company']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for item in csv_data:
+            writer.writerow({'first_name': item['contactFirstname'],
+                            'last_name': item['contactLastname'],
+                            'company': item['contactCompany']})
+
 def main(argv):
 
     parser = OptionParser()
-    parser.add_option("-i", "--inputfile", dest="inputfile",
-                  help="CSV file which contains the data to import")
+    parser.add_option("-d", "--dir", dest="input_dir",
+                  help="Directory which contains the CSV files to import")
+    parser.add_option("-o", "--outputfile", dest='output_file',
+                  help="CSV file to export the data into",
+                      default='export_data.csv')
     parser.add_option("-u", "--username", dest='username',
                   help="RG username")
     parser.add_option("-p", "--password", dest='password',
@@ -106,7 +126,7 @@ def main(argv):
 
     (options, args) = parser.parse_args()
 
-    if options.inputfile is None or options.username is None or options.password is None or options.account is None:
+    if options.input_dir is None or options.username is None or options.password is None or options.account is None:
         parser.error("Missing arguments")
         sys.exit(1)
 
@@ -114,10 +134,10 @@ def main(argv):
     if token is None:
         sys.exit(1)
 
-    data = import_data_file(options.inputfile)
+    data = import_data_file(options.input_dir)
     updated_company_list = merge_contacts(token, tokenHash, data)
-    pp.pprint(updated_company_list)
-
+    if updated_company_list is not None:
+        store_data(os.path.join(options.input_dir, options.output_file), updated_company_list)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
