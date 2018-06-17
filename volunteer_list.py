ac7a3612e9a466cf6460c84efd37385ed7ec5eb3
Tracy Jones <tjones@vmware.com>
2018-06-17 15:47:49 -0700

    lotsa changes

--- volunteer_list.py
+++ volunteer_list.py
@@ -12,6 +12,7 @@ import sys
 
 pp = pprint.PrettyPrinter()
 
+
 def login(username, password, account):
     login_data = {
         "username": username,
@@ -21,7 +22,8 @@ def login(username, password, account):
     }
 
     print json.dumps(login_data)
-    r = requests.post('https://api.rescuegroups.org/http/v2.json', data = json.dumps(login_data))
+    r = requests.post('https://api.rescuegroups.org/http/v2.json',
+                      data=json.dumps(login_data))
     if r.status_code == 200:
         content = json.loads(r.content)
         if content['status'] == 'ok':
@@ -32,45 +34,195 @@ def login(username, password, account):
         print r.text
     return (None, None)
 
-
-def get_volunteers(token, tokenHash, csv_data):
-
+def get_data(data, processor_func, limit=100, verbose=False):
     start = 0
-    limit = 100
-    updated_company_list = []
+    val = []
     while (1):
-        data = {
-                "token" : token,
-                "tokenHash" : tokenHash,
-                "objectType" : "contacts",
-                "objectAction" : "search",
-                "search" :  {
-                        "resultStart" : start,
-                        "resultLimit" : limit,
-                        "resultSort" : "contactLastname",
-                        "resultOrder" : "asc",
-                        "fields" : [ "contactFirstname","contactLastname",
-                                     "contactEmail", "contactCompany", "contactGroups"]
-                }
-            }
-        r = requests.post('https://api.rescuegroups.org/http/v2.json', data = json.dumps(data))
+        r = requests.post('https://api.rescuegroups.org/http/v2.json',
+                          data=json.dumps(data))
         if r.status_code == 200:
             content = json.loads(r.content)
             if content['status'] == 'ok':
-                for key,val in content['data'].items():
-                    if 'Jones' in val['contactLastname']:
-                        print val
-                    if 'Volunteer' in val['contactGroups']:
-                        csv_data.append(val)
+
+                if processor_func is not None:
+                    val = processor_func(content, val)
+
+                if verbose:
+                    for key, val in content['data'].items():
+                        print key, val
                 start += limit
-                if start > int(content['foundRows']):
-                    return csv_data
+                if start >= int(content['foundRows']):
+                    return val
             else:
                 print content['messages']
                 return
         else:
             print r.text
             return
+
+def get_animal_id(token, tokenHash, animal_name, animal_species):
+    limit = 10
+    data = {
+        "token": token,
+        "tokenHash": tokenHash,
+
+        "objectType": "animals",
+        "objectAction": "search",
+        "search":
+            {
+                "resultStart": 0,
+                "resultLimit": limit,
+                "resultSort": "animalID",
+                "resultOrder": "asc",
+                "filters":
+                    [
+                        {
+                            "fieldName": "animalStatus",
+                            "operation": "equals",
+                            "criteria": "hold"
+                        },
+                        {
+                            "fieldName": "animalSpecies",
+                            "operation": "equals",
+                            "criteria": animal_species
+                        },
+                        {
+                            "fieldName": "animalName",
+                            "operation": "equals",
+                            "criteria": animal_name
+                        },
+                    ],
+                "filterProcessing": "3",
+                "fields":
+                    [
+                        "animalID", "animalName", "animalStatus",
+                        "animalSpecies"]
+            }
+    }
+
+    def process_data(content, data):
+        if content['foundRows'] != '1':
+            print 'Found multiple matches'
+            for key, val in content['data'].items():
+                print key, val
+            return
+        else:
+            for key, val in content['data'].items():
+                print key, val
+                data.append(key)
+                return data
+
+    return get_data(data, process_data, limit)
+
+def get_file_list(token, tokenHash, id):
+    limit = 50
+
+    data = {
+        "token": token,
+        "tokenHash": tokenHash,
+
+        "objectType": "animalFiles",
+        "objectAction": "search",
+        "search":
+            {
+                "resultStart": 0,
+                "resultLimit": limit,
+                "resultSort": "animalfileID",
+                "resultOrder": "asc",
+                "filters":
+                    [
+                        {
+                            "fieldName": "animalfileAnimalID",
+                            "operation": "equals",
+                            "criteria": id
+                        },
+                     ],
+                "filterProcessing": "1",
+                "fields":
+                    [
+                        "animalfileID",
+                        "animalfileAnimalID",
+                        "animalfileOldName",
+                        "animalfileDescription",
+                        "animalfileStatus",
+                        "animalfileDisplayInline",
+                        "animalfilePublic",
+                        "animalfileSize",
+                        "animalfileCreatedDate", ]
+            }
+    }
+
+    def process_data(content, file_list):
+        for key, val in content['data'].items():
+            if val['animalfileOldName'].lower().find('internal') == -1:
+                file_list.append(val['animalfileID'])
+        return file_list
+
+    return get_data(data, process_data, limit)
+
+
+# def get_file(token, tokenHash, file_id):
+#     start = 0
+#     limit = 50
+#     while (1):
+#         data = {
+#             "token": token,
+#             "tokenHash": tokenHash,
+#
+#             "objectType": "animalFiles",
+#             "objectAction": "view",
+#             "values":
+#                 [
+#                     {
+#                         "animalfileID": file_id
+#                     }
+#
+#                 ],
+#             "fields": ["animalfileID", "animalfileAnimalID",
+#                        "animalfileOldName", "animalfileDescription",
+#                        "animalfileStatus", "animalfileDisplayInline",
+#                        "animalfileSize", "animalfileCreatedDate"]
+#         }
+#
+#         r = requests.post('https://api.rescuegroups.org/http/v2.json',
+#                           data=json.dumps(data))
+#         if r.status_code == 200:
+#             content = json.loads(r.content)
+#             if content['status'] == 'ok':
+#                 file_list = []
+#                 for key, val in content['data'].items():
+#                     if val['animalfileOldName'].lower().find('internal') == -1:
+#                         file_list.append(val['animalfileID'])
+#                     #print key, val
+#                 start += limit
+#                 if start > int(content['foundRows']):
+#                     return file_list
+#             else:
+#                 print content['messages']
+#                 return
+#         else:
+#             print r.text
+#             return
+
+def show_data(token, tokenHash):
+    start = 0
+    limit = 100
+
+    data = {
+        "token": token,
+        "tokenHash": tokenHash,
+        "objectType": "volunteersJournalEntries",
+        "objectAction": "search",
+        "search": {
+            "resultStart": start,
+            "resultLimit": limit,
+            "resultSort": "volunteerName",
+            "resultOrder": "asc",
+            "fields": ["volunteerName", "journalEntryComment"]
+        }
+    }
+    get_data(data, processor_func=None, verbose=True)
+
 
 def store_data(output_file, csv_data):
     with open(output_file, 'w') as csvfile:
@@ -80,37 +232,55 @@ def store_data(output_file, csv_data):
         writer.writeheader()
         for item in csv_data:
             writer.writerow({'first_name': item['contactFirstname'],
-                            'last_name': item['contactLastname'],
-                            'email': item['contactEmail'],
-                            'company': item['contactCompany']})
+                             'last_name': item['contactLastname'],
+                             'email': item['contactEmail'],
+                             'company': item['contactCompany']})
+
 
 def main(argv):
-
     parser = OptionParser()
     parser.add_option("-o", "--outputfile", dest='output_file',
-                  help="CSV file to export the data into",
+                      help="CSV file to export the data into",
                       default='export_data.csv')
     parser.add_option("-u", "--username", dest='username',
-                  help="RG username")
+                      help="RG username")
     parser.add_option("-p", "--password", dest='password',
-                  help="RG password")
+                      help="RG password")
     parser.add_option("-a", "--account", dest='account',
-                  help="RG account")
+                      help="RG account")
+    parser.add_option("-n", "--name", dest='name',
+                      help="Animal Name")
+    parser.add_option("-s", "--species", dest='species',
+                      help="Animal Species")
 
     (options, args) = parser.parse_args()
 
-    if options.username is None or options.password is None or options.account is None:
+    if options.username is None or options.password is None or \
+                    options.account is None or options.name is None or \
+                    options.species is None:
         parser.error("Missing arguments")
         sys.exit(1)
 
-    (token, tokenHash) = login(options.username, options.password, options.account)
+    (token, tokenHash) = login(options.username, options.password,
+                               options.account)
     if token is None:
         sys.exit(1)
 
-    data = []
-    volunteer_list = get_volunteers(token, tokenHash, data)
-    if volunteer_list is not None:
-        store_data(options.output_file, volunteer_list)
+    pp =  pprint.PrettyPrinter(indent=4)
+    id = get_animal_id(token, tokenHash, options.name, options.species)
+    if id is not None:
+        print id
+        file_list = get_file_list(token, tokenHash, id)
+        pp.pprint(file_list)
+    #    for file in file_list:
+    #        get_file(token, tokenHash, file)
+    #    if volunteer_list is not None:
+    #         store_data(options.output_file, volunteer_list)
+
+    show_data(token, tokenHash)
+
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
