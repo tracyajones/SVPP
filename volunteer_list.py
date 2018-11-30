e9c9c9c30b67d8054e3a3c9378fa631d2621b0af
Tracy Jones <tjones@vmware.com>
2018-11-30 09:54:01 -0800

    start to refactor

--- volunteer_list.py
+++ volunteer_list.py
@@ -9,56 +9,10 @@ import os
 import pprint
 import requests
 import sys
+import utils
 
 pp = pprint.PrettyPrinter()
 
-
-def login(username, password, account):
-    login_data = {
-        "username": username,
-        "password": password,
-        "accountNumber": account,
-        "action": "login"
-    }
-
-    print json.dumps(login_data)
-    r = requests.post('https://api.rescuegroups.org/http/v2.json',
-                      data=json.dumps(login_data))
-    if r.status_code == 200:
-        content = json.loads(r.content)
-        if content['status'] == 'ok':
-            return (content['data']['token'], content['data']['tokenHash'])
-        else:
-            print content['message']
-    else:
-        print r.text
-    return (None, None)
-
-def get_data(data, processor_func, limit=100, verbose=False):
-    start = 0
-    val = []
-    while (1):
-        r = requests.post('https://api.rescuegroups.org/http/v2.json',
-                          data=json.dumps(data))
-        if r.status_code == 200:
-            content = json.loads(r.content)
-            if content['status'] == 'ok':
-
-                if processor_func is not None:
-                    val = processor_func(content, val)
-
-                if verbose:
-                    for key, val in content['data'].items():
-                        print key, val
-                start += limit
-                if start >= int(content['foundRows']):
-                    return val
-            else:
-                print content['messages']
-                return
-        else:
-            print r.text
-            return
 
 def get_animal_id(token, tokenHash, animal_name, animal_species):
     limit = 10
